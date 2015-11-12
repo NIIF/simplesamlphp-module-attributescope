c9937477e5ca8aaf1ac5cc80fe9f80b53bf5efc7
Szabó Gyula <gyufi@niif.hu>
2015-11-12 16:28:32 +0100

    schacHomeOrganization, regexped scopes, filtering

--- lib/Auth/Process/FilterAttributes.php
+++ lib/Auth/Process/FilterAttributes.php
@@ -30,16 +30,22 @@ class sspmod_attributescope_Auth_Process_FilterAttributes extends SimpleSAML_Aut
     public function process(&$request)
     {
         $src = $request['Source'];
+        $noscope = false;
         if (!isset($src['scope']) ||
                 !is_array($src['scope']) ||
                 !count($src['scope'])) {
-            SimpleSAML_Logger::warning("The IdP does not have scope! entityId: ". $src['entityid']);
-            return;
+            SimpleSAML_Logger::warning("The IdP does not have scope, filter out all scoped attributes! entityId: ". $src['entityid']);
+            $noscope = true;
         }
         $scopes = $src['scope'];
 
         foreach ($this->scopedattributes as $scopedattribute) {
             if (!isset($request['Attributes'][$scopedattribute])) {
+                continue;
+            }
+            if ($noscope) {
+                SimpleSAML_Logger::info("The IdP does not have scope! Attribute ". $scopedattribute ." filtered out. ");
+                unset($request['Attributes'][$scopedattribute]);
                 continue;
             }
             $values = $request['Attributes'][$scopedattribute];
@@ -51,7 +57,22 @@ class sspmod_attributescope_Auth_Process_FilterAttributes extends SimpleSAML_Aut
                     SimpleSAML_Logger::warning("Attribute value $value is not properly scoped");
                 }
             }
-            $request['Attributes'][$scopedattribute] = $newValues;
+
+            if (count($newValues)) {
+                $request['Attributes'][$scopedattribute] = $newValues;
+            } else {
+                unset($request['Attributes'][$scopedattribute]);
+            }
+        }
+        // Filter out schacHomeOrganization if the value not match to any scope value
+        if (array_key_exists('schacHomeOrganization', $request['Attributes'])) {
+            if (count($request['Attributes']['schacHomeOrganization']) != 1) {
+                    SimpleSAML_Logger::warning("Too much schacHomeOrganization attribute element. Only one allowed. Filtered out.");
+                    unset($request['Attributes']['schacHomeOrganization']);
+            } elseif (! in_array($request['Attributes']['schacHomeOrganization'][0], $scopes)) {
+                SimpleSAML_Logger::warning("There is no schacHomeOrganization in scopes array. Filtered out.");
+                unset($request['Attributes']['schacHomeOrganization']);
+            }
         }
     }
 
@@ -65,10 +86,28 @@ class sspmod_attributescope_Auth_Process_FilterAttributes extends SimpleSAML_Aut
     private function isProperlyScoped($value, $scopes)
     {
         foreach ($scopes as $scope) {
-            $preg = '/^[^@]*@' . preg_quote($scope). '$/';
+            if ($this->isScopeRegexp($scope)) {
+                $preg = $scope;
+            } else {
+                $preg = '/^[^@]*@' . preg_quote($scope). '$/';
+            }
             if (preg_match($preg, $value) == 1) {
                 return true;
             }
         }
+    }
+
+    /**
+     * Is scope regexp or not. We cant get it from 'Source'
+     * @param  string  $scopetext the value of scope
+     * @return boolean
+     */
+    private function isScopeRegexp($scopetext)
+    {
+        // Check regexp as SimpleSAML_Metadata_SAMLBuilder do
+        if (1 === preg_match('/[\$\^\)\(\*\|\\\\]/', $scopetext)) {
+            return true;
+        }
+        return false;
     }
 }
